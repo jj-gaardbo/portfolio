5a84a3001c3e09ca0081303415c6ae40bab2d3a2
jj-gaardbo <jgaard16@gmail.com>
2019-04-09 22:42:50 +0200

    Small container wrap on project page

--- src/styles/pages/project.js
+++ src/styles/pages/project.js
@@ -1,5 +1,5 @@
 import { css } from '@emotion/core'
-//import * as constant from '../base/constants'
+import * as constant from '../base/constants'
 import globalStyles from '../base/global'
 
 export default css`
--- src/templates/project.js
+++ src/templates/project.js
@@ -8,12 +8,11 @@ const ProjectPage = ({ pageContext }) => {
   return (
     <Layout styles={projectTemplateStyles}>
       <SEO title="Gaardbo Digital - Projects" keywords={[`projects`, `overview`, `gaardbo digital`]} />
-      <article>
-        <div dangerouslySetInnerHTML={{__html: pageContext.content}}></div>
-        <Link to="/projects">Go back to the projects page</Link>
-        <hr/>
-        <Link to="/">Go back to the homepage</Link>
-      </article>
+      <section>
+        <div className="container">
+          <div dangerouslySetInnerHTML={{__html: pageContext.content}}></div>
+        </div>
+      </section>
     </Layout>
   )
 }
