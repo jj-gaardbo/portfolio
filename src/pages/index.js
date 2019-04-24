6de3f7c6c292efc544267cbba8087eae2dd9f765
jj-gaardbo <jgaard16@gmail.com>
2019-04-25 01:18:48 +0200

    Added frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,22 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import frontPageStyle from '../styles/pages/frontpage'
-import { QuickLink } from "../styles/pages/frontpage"
 import { graphql } from "gatsby"
 import * as constant from '../styles/base/constants'
 import { css, Global } from "@emotion/core"
+import Image from "../components/Image"
+import { getRandomComicPattern, invertColor } from "../util/common"
+import Svg from "../components/Svg"
+import pattern from "../images/patterns/4.svg"
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 
 const IndexPage = ({ data }) => {
+
+  const content = data.text.content
+  const settingsNode = JSON.parse(data.settings.content)
+
   return(
-    <Layout styles={frontPageStyle} backgroundColor={constant.darkWhite} color={constant.black}>
+    <Layout styles={frontPageStyle} backgroundColor={settingsNode.color} color={invertColor(settingsNode.color)}>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <Global
         styles={css`
@@ -19,13 +27,31 @@ const IndexPage = ({ data }) => {
 				`}
       />
 
-      <QuickLink>
+      <Svg width={442} height={500} svg={pattern}></Svg>
 
-        <div className="container">
-          <div dangerouslySetInnerHTML={{__html: data.fileName.content}}></div>
-        </div>
+      <ParallaxProvider>
 
-      </QuickLink>
+        <section className='frontpage-section'>
+
+          <div className="container">
+
+            <div className="text">
+
+              <div dangerouslySetInnerHTML={{__html: content}}></div>
+
+            </div>
+
+            <Parallax className="custom-class" y={[10, 10]} x={[-10, 25]} tagOuter="figure">
+
+              <Image path={settingsNode.mainImg} alt={"Profile Image"}/>
+
+            </Parallax>
+
+          </div>
+
+        </section>
+
+      </ParallaxProvider>
 
     </Layout>
   )
@@ -34,11 +60,18 @@ const IndexPage = ({ data }) => {
 
 export const query = graphql`
 query IndexPageQuery {
-  fileName: file(relativePath: {eq: "content.html"}) {
-    base
-    relativeDirectory
-    content
-  }
+  text: 
+    file(relativePath: {eq: "content.html"}) {
+      base
+      relativeDirectory
+      content
+  },
+  settings:
+    file(relativePath: {eq: "settings.json"}) {
+      base
+      relativeDirectory
+      content
+    }
 }
 `
 
--- src/styles/pages/frontpage.js
+++ src/styles/pages/frontpage.js
@@ -1,19 +1,86 @@
 import { css } from '@emotion/core'
-import styled from '@emotion/styled'
-//import * as constant from '../base/constants'
-import globalStyles from '../base/global'
+import * as constant from '../base/constants'
+import globalStyles, { containerLayout } from "../base/global"
 
 export default css`
   ${globalStyles}
+  ${containerLayout}
+  position: relative;
   
+  svg{
+    position: absolute;
+    max-width: 100%;
+    width: 100vw;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+    opacity: 0.1;
+  }
   
-`
-
-export const QuickLinkStyle = css`
-  display: grid;
-  grid-column: -1 / 1;
-`
-
-export const QuickLink = styled.div`
-		${QuickLinkStyle}
+  section{
+    padding: ${constant.padMobile} 0 0;
+    display: grid;
+    grid-template-rows: auto;
+    
+    .container{
+      position: relative;
+    }
+    
+    .text{
+      position: static;
+      width: 100%;
+      background-color: ${constant.darkWhite};
+      border: 10px solid ${constant.black};
+      box-sizing: border-box;
+      padding: 20px;
+    }
+    
+    .parallax-outer{
+      max-width: 100%;
+      overflow: hidden;
+    }
+    
+    .parallax-inner{
+      display: grid;
+      max-width: 100%;
+      overflow: hidden;
+      
+      .gatsby-image-wrapper{ 
+        max-width: 100%;
+        img{
+          width: 150px;
+          -webkit-filter: drop-shadow(5px 5px 5px #222);
+          filter: drop-shadow(5px 5px 5px #222);
+        }
+        
+      }  
+      
+    }
+  }
+  
+  ${constant.md}{
+    section{
+      .text{
+        position: absolute;
+        left: 0;
+        top: 10px;
+        z-index: 2;
+        max-width: 450px;
+      }
+    
+      .gatsby-image-wrapper{
+        justify-self: center;
+        width: 800px;
+      }
+    }
+  }
+  
+  ${constant.lg}{
+    section{
+      .text{
+        max-width: 550px;
+      }
+    }
+  }
+  
 `
